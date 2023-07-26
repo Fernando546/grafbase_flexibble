7d6a6aab7af5e5a188bf8b6ec308d12c17a3181e
Dawid <ferero546@gmail.com>
2023-07-26 20:45:00 +0200

    common.types.ts

---
+++ components/ProjectForm.tsx
@@ -0,0 +1,15 @@
+import { SessionInterface } from '@/common.types';
+import React from 'react'
+
+type Props = {
+  type: string;
+  session: SessionInterface;
+}
+
+const ProjectForm = ({ type, session }: Props) => {
+  return (
+    <div>ProjectForm</div>
+  )
+}
+
+export default ProjectForm
